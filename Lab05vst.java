d7c458ab89b9f6ecc41a5f59656339c8c96249ec
Antonio Martin <antonio.martin@vtsd.com>
2022-10-28 11:16:37 -0400

    Lab05

--- Lab05vst.java
+++ Lab05vst.java
@@ -10,7 +10,7 @@ public class Lab05vst extends Applet {
     public void paint(Graphics g) {
         int width = 980;
         int height = 630;
-        int x1 = 1;
+        int x1 = 10;
         int y1 = 640;
         int x2 = 990;
         int y2 = 640;
@@ -19,9 +19,8 @@ public class Lab05vst extends Applet {
 
         for (int x = 1; x <= 60; x++){
             g.drawLine(x1,y1,x2,y2);
-
-
-
+            x1 += 20;
+            y2 -= 20;
 
         }
 
