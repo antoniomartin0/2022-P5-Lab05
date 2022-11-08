d3796994ca3b22535015c04f78dcf380d0c34660
Antonio Martin <antonio.martin@vtsd.com>
2022-11-08 12:28:05 -0500

    Lab05

--- Temp.java
+++ Temp.java
@@ -1,14 +1,15 @@
 import java.applet.Applet;
 import java.awt.*;
 import java.util.Random;
-public class Temp extends Applet{
+public class Temp extends Applet {
     public void paint(Graphics g) {
         Random rand = new Random(7442);
+        int x1 = rand.nextInt(1000);
+        int y1 = rand.nextInt(650);
+        int x2 = rand.nextInt(1000);
+        int y2 = rand.nextInt(650);
+        g.drawLine(x1, y1, x2, y2);
 
-        int x1 = rand.nextInt();
-        int y1 = rand.nextInt();
-        int x2 = rand.nextInt();
-        int y2 = rand.nextInt();
-        g.drawLine(x1,y1,x2,y2);
+        }
     }
 }
